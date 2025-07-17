2a2e582281cc470bacb043847841fbdcf26e8b5d
Joshua-White-NeuroForged <joshua.white@neuroforged.com>
2025-07-17 13:59:57 +0100

    removed joshs email for now

--- src/main/java/com/neuroforged/leadsystem/service/LeadServiceImpl.java
+++ src/main/java/com/neuroforged/leadsystem/service/LeadServiceImpl.java
@@ -60,7 +60,6 @@ Traffic Sourece: \{lead.getTrafficSource()}
 ClientId: \{lead.getClientId()}
 Created at:\{lead.getCreatedAt()}""");
         try {
-            emailService.sendLeadNotification("joshua.white@neuroforged.com", subject, body);
             emailService.sendLeadNotification("matthew.mcfarlane@neuroforged.com", subject, body);
         } catch (MessagingException e) {
             e.printStackTrace(); // You might log this properly
