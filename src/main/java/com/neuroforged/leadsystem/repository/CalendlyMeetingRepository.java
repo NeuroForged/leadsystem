d0ee63b6dc464e7009ea2023c8d80681cc1f3f4f
Joshua White <joshua.white@neuroforged.com>
2025-08-27 10:39:33 -0700

    feat(repository): add findByCalendlyUriAndStartTime method to CalendlyMeetingRepository

--- src/main/java/com/neuroforged/leadsystem/repository/CalendlyMeetingRepository.java
+++ src/main/java/com/neuroforged/leadsystem/repository/CalendlyMeetingRepository.java
@@ -2,6 +2,9 @@ package com.neuroforged.leadsystem.repository;
 
 import com.neuroforged.leadsystem.entity.CalendlyMeeting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import java.time.ZonedDateTime;
 
 public interface CalendlyMeetingRepository extends JpaRepository<CalendlyMeeting, Long> {
+    Optional<CalendlyMeeting> findByCalendlyUriAndStartTime(String calendlyUri, ZonedDateTime startTime);
 }
