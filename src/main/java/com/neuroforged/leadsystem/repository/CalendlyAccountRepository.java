b4f8c77b67b8d616db3e34e710a9197e6d09b622
Joshua White <joshua.white@neuroforged.com>
2025-08-27 10:35:22 -0700

    fix(repository): correct interface definition and replace broken lines in CalendlyAccountRepository

--- src/main/java/com/neuroforged/leadsystem/repository/CalendlyAccountRepository.java
+++ src/main/java/com/neuroforged/leadsystem/repository/CalendlyAccountRepository.java
@@ -4,11 +4,9 @@ import com.neuroforged.leadsystem.entity.CalendlyAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
 import java.util.List;
 
-public interface CalendlyAccountRe
-        List<CalendlyAccount> findByPollEnabledTrue();
-pository extends JpaRepository<CalendlyAccount, Long> {
+public interface CalendlyAccountRepository extends JpaRepository<CalendlyAccount, Long> {
+    List<CalendlyAccount> findByPollEnabledTrue();
     Optional<CalendlyAccount> findByClientId(Long clientId);
 }
