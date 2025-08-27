163221f017827c9f6663d97e1f469462130ff72c
Joshua White <joshua.white@neuroforged.com>
2025-08-27 10:23:21 -0700

    feat(calendly): add scheduled events and invitees methods to CalendlyApiClient

--- src/main/java/com/neuroforged/leadsystem/client/CalendlyApiClient.java
+++ src/main/java/com/neuroforged/leadsystem/client/CalendlyApiClient.java
@@ -1,4 +1,10 @@
 package com.neuroforged.leadsystem.client;
+import java.time.Instant;
+import java.util.List;
+import com.neuroforged.leadsystem.entity.CalendlyAccount;
+import com.neuroforged.leadsystem.dto.ScheduledEventDTO;
+import com.neuroforged.leadsystem.dto.InviteeDTO;
+
 
 import com.neuroforged.leadsystem.dto.CalendlyTokenResponse;
 
@@ -12,5 +18,10 @@ public interface CalendlyApiClient {
     /**
      * Exchanges the authorization code for an access + refresh token.
      */
-    CalendlyTokenResponse exchangeCodeForToken(String code);
-}
+  
+      CalendlyTokenResponse exchangeCodeForToken(String code);
+
+    List<ScheduledEventDTO> listScheduledEvents(CalendlyAccount account, Instant minStartTime, Instant maxStartTime);
+
+    List<InviteeDTO> listInvitees(CalendlyAccount account, String scheduledEventUuid);
+
